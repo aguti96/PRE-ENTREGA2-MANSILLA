0a2c9c568da540e7ee4dc6a12da9b6125254a3d5
Agustin Mansilla <mansillaagustin6@gmail.com>
2023-08-08 19:56:19 -0700

    arreglando errores

--- preentrega/src/App.js
+++ preentrega/src/App.js
@@ -1,31 +1,27 @@
-import "./App.css"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Navbar from './components/NavBar/NavBar';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import { Cart } from "./components/Cart/Cart";
-import { CartProvider } from "./context/CartContext"
-
-
-
+import { CartProvider } from "./context/CartContext";
 
 function App() {
-
-      <BrowserRouter>
-        <CartProvider>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<ItemListContainer />} />
-            <Route path="/category/:categoryId" element={<ItemListContainer />} />
-            <Route path="/item/:itemId" element={<ItemDetailContainer />} />
-            <Route path="/cart" element={<Cart />} /> 
-            <Route path="*" element={<h1>404 NOT FOUND</h1>} />
-          </Routes>
-        </CartProvider>
-      </BrowserRouter>
-    
-  
+  return (
+    <BrowserRouter>
+      <CartProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/category/:categoryId" element={<ItemListContainer />} />
+          <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<h1>404 NOT FOUND</h1>} />
+        </Routes>
+      </CartProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- preentrega/src/components/ItemListContainer/ItemListContainer.js
+++ preentrega/src/components/ItemListContainer/ItemListContainer.js
@@ -1,44 +1,47 @@
-import { useState, useEffect } from "react"  
-import { getDocs, collection, query, where } from "firebase/firestore"
-import ItemList from "../ItemList/ItemList"
-import { useParams } from "react-router-dom"
-import { db } from "../../services/firebase/firebase";
+import { useState, useEffect } from "react";  
+import { getDocs, collection, query, where } from "firebase/firestore";
+import ItemList from "../ItemList/ItemList";
+import { useParams } from "react-router-dom";
+import { db } from "../../services/firebase/firebaseConfig";
 
+const ItemListContainer = () => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-
-const ItemListContainer = ({ greeting }) => {
-    const [products, setProducts] = useState([])
-    const [loading, setLoading] = useState(true)
-
-    const { categoryId } = useParams()
+    const { categoryId } = useParams();
 
     useEffect(() => {
-        setLoading(true)
+        setLoading(true);
 
         const collectionRef = categoryId
             ? query(collection(db, "Items"), where("category", "==", categoryId))
-            : collection(db, "Items")
+            : collection(db, "Items");
         
-            
         getDocs(collectionRef)
             .then(response => {
                 const productsAdapted = response.docs.map(doc => {
-                    const data = doc.data()
-                    return { id: doc.id, ...data }
-                })
-                setProducts(productsAdapted)
+                    const data = doc.data();
+                    return { id: doc.id, ...data };
+                });
+                setProducts(productsAdapted);
             })
             .catch(error => {
-                console.log(error)
+                console.log(error);
             })
             .finally(() => {
-                setLoading(false)
-            })    
+                setLoading(false);
+            });    
+    }, [categoryId]); 
 
-    })
+    return (
+        <div>
+            {loading ? (
+                <p>Cargando...</p>
+            ) : (
+                <ItemList products={products} />
+            )}
+        </div>
+    );
+};
 
-
-    }
-
-
-export default ItemListContainer
+export default ItemListContainer;
